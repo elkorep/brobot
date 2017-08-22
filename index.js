3ac26b56e4cae4432aff7802fef19259ede80f23
Peter El-koreh <el-koreh@ca.ibm.com>
2017-08-22 18:09:15 -0400

    post message with attachment

--- index.js
+++ index.js
@@ -6,29 +6,33 @@ var fs = require('fs');
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 var RTM_CLIENT = require('@slack/client').RtmClient;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+var WebClient = require('@slack/client').WebClient;
 var config = require('./config.json');
 
 var botToken = config.botToken;
 var channel = config.slackchannel;
 var rtm = new RTM_CLIENT(botToken);
+var webApi = new WebClient(config.webapitoken);
+var brobotName = config.brobotName;
 
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function(rtmStartData) {
   console.log('Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}');
 });
 
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
-  var lcMessage = message.text.toLowerCase();
-  if (lcMessage.indexOf('bro') > -1 || lcMessage.indexOf('brobot') > -1) {
-    if(lcMessage.indexOf('ptal') > -1) {
-      ptal(message);
-    }
-    else if (lcMessage.indexOf('reviews') > -1) {
-      reviews();
-    }
-
-    else if (lcMessage.indexOf('help') > -1) {
-      var help = fs.readFileSync('./help.txt').toString();
-      rtm.sendMessage(help, channel);
+  if(message.text){
+    var lcMessage = message.text.toLowerCase();
+    if (message.text.indexOf(brobotName) > -1 || lcMessage.indexOf('bro') > -1 || lcMessage.indexOf('brobot') > -1) {
+      if(lcMessage.indexOf('ptal') > -1) {
+        ptal(message);
+      }
+      else if(lcMessage.indexOf('reviews') > -1) {
+        reviews();
+      }
+      else if (lcMessage.indexOf('help') > -1) {
+        var help = fs.readFileSync('./help.txt').toString();
+        rtm.sendMessage(help, channel);
+      }
     }
   }
 });
@@ -40,9 +44,10 @@ function ptal(message) {
   var reviewers = [];
   var pr = '';
   var text = '';
+  var reviewExists = false;
 
   splitMessage.forEach(function(word) {
-    if(word.indexOf('@') > -1) reviewers.push(word);
+    if(word.indexOf('@') > -1 && word.indexOf(brobotName) === -1) reviewers.push(word);
     else if(word.indexOf('https://github') > -1 && word.indexOf('pull') > -1) pr = word;
   });
 
@@ -51,25 +56,33 @@ function ptal(message) {
       text += reviewer + ' ';
     });
     text += pr;
-    attachment.text = text;
-    attachment.callback_id = pr;
-    reviews.attachments.push(attachment);
-
-    fs.writeFile('./reviews.json', reviews, function(err) {
-      if(err) {
-        return console.log(err);
-      }
-      console.log("reviews.json overwritten");
+    reviews.attachments.forEach(function(attachment){
+      reviewExists = attachment.text.indexOf(pr) > -1;
     });
+
+    if(!reviewExists) {
+      attachment.text = text;
+      reviews.attachments.push(attachment);
+      fs.writeFile('./reviews.json', JSON.stringify(reviews), function(err) {
+        if(err) {
+          return console.log(err);
+        }
+        console.log('reviews.json overwritten');
+      });
+    }
   }
 }
 
 function reviews() {
-    var reviews = require('./reviews.json');
-    if(reviews.attachments.length > 0) {
-      return rtm.sendMessage(reviews, channel);
-    }
-    return rtm.sendMessage('No Reviews Found');
+  var reviews = require('./reviews.json');
+  if(reviews.attachments.length > 0) {
+    return webApi.chat.postMessage(channel, 'Reviews', {attachments: JSON.stringify(reviews.attachments)}, function(err) {
+      if (err) {
+        console.log('Error:', err);
+      }
+    });
+  }
+  return rtm.sendMessage('No Reviews Found', channel);
 }
 
 rtm.start();
